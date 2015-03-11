17777e6f01ab5a3a198b8be2a35304baddb597b2
iwanglian <iwanglian@gmail.com>
2015-03-11 18:00:24 +0800

    rename png to 3x png

---
+++ rename3x.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+	import sys, os
+	root = sys.argv[1]
+	for filename in os.listdir(root):
+		if(filename.endswith(".png") and not filename.endswith("@3x.png")):
+			#print filename[:-4]
+			fullpath = os.path.join(root,filename)
+			fullpath2 = os.path.join(root,"@3x".join([filename[:-4],".png"]))
+			os.rename(fullpath, fullpath2)
